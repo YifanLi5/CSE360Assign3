66c45a2f79dcdc980a5f5341ec6f13ba301d7ac1
YifanLi5 <yifanli5@asu.edu>
2016-02-22 12:36:06 -0700

    initial check

---
+++ src/cse360assign3/calc.java
@@ -0,0 +1,65 @@
+package cse360assign3;
+/** Basic calculator class
+ * @author Yifan Li
+ * @version Fed 22, 2016
+ * 
+ */
+
+class Calculator {
+
+	private int total;
+	
+	/**
+	 * initialize total to 0
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * returns the total 
+	 * @return value of total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * add this value to total
+	 * @param value: number to add
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract this value from total
+	 * @param value: number to subtract
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply this value to the total
+	 * @param value: number to multiply 
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide ths total by this value
+	 * @param value: value to divide
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * placeholder method
+	 * @return not needed yet, placeholder
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ src/cse360assign3/testmain.java
@@ -0,0 +1,10 @@
+package cse360assign3;
+
+public class testmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
